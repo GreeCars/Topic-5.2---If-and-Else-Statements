79a7ce97c0aed1682c9ca935862508e03dffc858
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-20 09:32:48 -0400

    Add project files.

---
+++ Topic 5.2 - If and Else Statements/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int grade;
+Console.WriteLine("What was your grade?");
+int.TryParse(Console.ReadLine(), out grade);
+if (grade >= 50)
+    Console.WriteLine("You Passed!");
+else
+    Console.WriteLine("Better luck next time");
